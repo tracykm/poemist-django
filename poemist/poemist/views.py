6acf87c7d38af6242ce893df2e96a53b8176c8ab
Tracy <tracykmullen@gmail.com>
2021-01-03 11:51:19 -0600

    test db connection

--- poemist/poemist/views.py
+++ poemist/poemist/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 def ping(request):
-    data = {'ping': 'pong!'}
+    data = {'ping': User.objects.first().email}
     return JsonResponse(data)
