4b6024a777a2506e3bcce79740b7f923d239e3af
Tracy <tracykmullen@gmail.com>
2021-01-03 11:05:47 -0600

    dockerfile

--- poemist/poemist/settings.py
+++ poemist/poemist/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-lwb%e00ihqb(aheig@b5)3aopvmhbu(lc9-km%2ka$53okg9g'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'poemist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- poemist/poemist/urls.py
+++ poemist/poemist/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import ping
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', ping, name="ping"),
 ]
+
+from django.urls import path
+
