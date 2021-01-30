b5eba2375f50b4f46f5041b42b5fb5d706a70d37
Tracy <tracykmullen@gmail.com>
2021-01-30 14:47:40 -0600

    change dir name

--- frontend/src/components/universal/DeleteEditLinks.tsx
+++ frontend/src/components/universal/DeleteEditLinks.tsx
@@ -4,12 +4,27 @@ import {
   GetPoemsDocument,
   useDeletePoemMutation,
 } from "src/queries/autogenerate/hooks"
+import produce from "immer"
+import { GetPoemsQuery } from "src/queries/autogenerate/operations"
 
 export default function DeleteEditLinks({ authorId, poemId }) {
   const isCurrentUser = true
   const [deletePoem] = useDeletePoemMutation({
     variables: { id: poemId },
     refetchQueries: [{ query: GetPoemsDocument }],
+    update: (cache, arg) => {
+      const data = cache.readQuery({ query: GetPoemsDocument })
+      const newData = produce(data, (draft: GetPoemsQuery) => {
+        var idx = draft.poemPages.edges.findIndex(
+          (d) => d.id === arg.data.deletePoem.id,
+        )
+        debugger
+        draft.poemPages.edges.splice(idx, 1)
+        debugger
+      })
+      debugger
+      cache.writeQuery({ query: GetPoemsDocument, data: newData })
+    },
   })
   return (
     <span className="delete-edit-links">
