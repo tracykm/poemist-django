89c31b717f19367667f1399d26efc5e6e7af06f0
Tracy <tracykmullen@gmail.com>
2021-03-26 08:36:17 -0500

    try breaking

---
+++ backend/db/conflict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys, os
+
+all_prefixes = {}
+for file in os.listdir("./migrations"):
+    prefix = file.split("_")[0]
+    if prefix:
+        if prefix in all_prefixes:
+            print("A migration conflict was found for", prefix)
+            sys.exit(1)
+        all_prefixes[prefix] = True
