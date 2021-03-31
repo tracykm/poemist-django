cb2bcda8404625393602d833227d40005a9cba36
Tracy <tracykmullen@gmail.com>
2021-03-30 19:13:24 -0500

    teest

---
+++ frontend/check_bundle_size.js
@@ -0,0 +1,36 @@
+const fs = require("fs")
+const path = require("path")
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, file))
+    }
+  })
+
+  return arrayOfFiles
+}
+
+const getTotalSize = function (directoryPath) {
+  const arrayOfFiles = getAllFiles(directoryPath)
+
+  let totalSize = 0
+
+  arrayOfFiles.forEach(function (filePath) {
+    totalSize += fs.statSync(filePath).size
+  })
+
+  return totalSize
+}
+
+const result = getTotalSize("./build")
+const js = getTotalSize("./build/static/js")
+const css = getTotalSize("./build/static/css")
+const media = getTotalSize("./build/static/media")
+console.log(result, { js, css, media })
