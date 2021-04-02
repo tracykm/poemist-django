dca9deff462ef159c3d7d42eb8c284689d145e23
Tracy <tracykmullen@gmail.com>
2021-04-02 14:42:11 -0500

    compare

---
+++ .github/workflows/compare.js
@@ -0,0 +1,12 @@
+var bundleSize = require("../../frontend/buildSize.json");
+var bundleSizeNew = require("../../frontend/buildSizeNew.json");
+
+const diffArray = Object.entries(bundleSize).map(([name, size]) => {
+  var newSize = bundleSizeNew[name];
+  return {
+    name,
+    size: newSize - size,
+  };
+});
+
+console.log(diffArray);
