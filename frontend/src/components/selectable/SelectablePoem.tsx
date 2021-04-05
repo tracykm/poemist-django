025331d5c1222c02733e337ad1ec8f17eed63cb2
Tracy <tracykmullen@gmail.com>
2021-04-05 10:39:04 -0500

    nudge

--- frontend/src/components/selectable/SelectablePoem.tsx
+++ frontend/src/components/selectable/SelectablePoem.tsx
@@ -1,11 +1,53 @@
-import { IHandleClickLetter } from "./Word"
-import PoemDiv from "src/components/poem/PoemDiv"
+import Word, { IHandleClickLetter } from "./Word"
+import styled from "styled-components"
+import PoemDiv, { BodyDiv } from "src/components/poem/PoemDiv"
 import { IWordLetter } from "../types"
 
-export default function SelectablePoemRender({}: {
+const SelectablePoemDiv = styled.div`
+  user-select: none;
+  .letter {
+    cursor: pointer;
+  }
+
+  .selecting-by-word .word:hover .letter,
+  .selecting-by-letter .letter:hover {
+    color: cyan;
+    &.is-selected {
+      color: red;
+    }
+  }
+`
+
+export default function SelectablePoemRender({
+  wordLetters,
+  isSelectingByWord,
+  handleClickLetter,
+}: {
   wordLetters: IWordLetter[][]
   isSelectingByWord: boolean
   handleClickLetter: IHandleClickLetter
 }) {
-  return <PoemDiv className="poem close-up" />
+  return (
+    <PoemDiv className="poem close-up">
+      <SelectablePoemDiv>
+        <BodyDiv className="poem-body">
+          <div className="background-img" />
+          <div
+            className={
+              isSelectingByWord ? "selecting-by-word" : "selecting-by-letter"
+            }
+          >
+            {wordLetters.map((word, i) => (
+              <Word
+                word={word}
+                key={i}
+                wordIdx={i}
+                handleClickLetter={handleClickLetter}
+              />
+            ))}
+          </div>
+        </BodyDiv>
+      </SelectablePoemDiv>
+    </PoemDiv>
+  )
 }
