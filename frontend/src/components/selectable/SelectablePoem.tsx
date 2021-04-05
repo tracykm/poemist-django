605f222130cd1766e143eff8f31de07b190385fd
Tracy <tracykmullen@gmail.com>
2021-04-05 10:16:04 -0500

    remove type errors

--- frontend/src/components/selectable/SelectablePoem.tsx
+++ frontend/src/components/selectable/SelectablePoem.tsx
@@ -1,23 +1,7 @@
-import Word, { IHandleClickLetter } from "./Word"
+import { IHandleClickLetter } from "./Word"
 import styled from "styled-components"
-import PoemDiv, { BodyDiv } from "src/components/poem/PoemDiv"
+import PoemDiv from "src/components/poem/PoemDiv"
 import { IWordLetter } from "../types"
-
-const SelectablePoemDiv = styled.div`
-  user-select: none;
-  .letter {
-    cursor: pointer;
-  }
-
-  .selecting-by-word .word:hover .letter,
-  .selecting-by-letter .letter:hover {
-    /* background: rgba(255, 255, 0, .7); */
-    color: cyan;
-    &.is-selected {
-      color: red;
-    }
-  }
-`
 
 export default function SelectablePoemRender({
   wordLetters,
